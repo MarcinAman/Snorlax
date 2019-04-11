2da938897df4f72950edc1aa871988c90f4110cd
Piotr Wojtyś <pituuuss@gmail.com>
2019-04-11 14:17:51 +0200

    Working Parser
    Add Rest Controller
    
    minor refactor
    
    Update SnorlaxApp.java
    
    Delete PoolController.java
    
    PR updates

--- src/main/java/com/mycompany/myapp/repository/ReservationRepository.java
+++ src/main/java/com/mycompany/myapp/repository/ReservationRepository.java
@@ -1,6 +1,5 @@
 package com.mycompany.myapp.repository;
 
-import com.mycompany.myapp.domain.Pool;
 import com.mycompany.myapp.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/mycompany/myapp/service/CSV/CSVParser.java
+++ src/main/java/com/mycompany/myapp/service/CSV/CSVParser.java
@@ -1,50 +1,47 @@
 package com.mycompany.myapp.service.CSV;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.mycompany.myapp.domain.Pool;
+import io.vavr.Tuple;
+import io.vavr.collection.List;
+import io.vavr.collection.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
+
 
 public class CSVParser {
     private static final Logger logger = LoggerFactory.getLogger(CSVParser.class);
 
-    public static void loadObjectList(String fileName) {
-        try {
-            CsvSchema bootstrapSchema =
-                CsvSchema.emptySchema().withHeader();
-////                    .builder().addArrayColumn("pool_id").addArrayColumn("displayName").addArrayColumn(
-////                    "maximumCount").addArrayColumn("enabled").addArrayColumn("description").build();
-//
-
-            File file = new ClassPathResource(fileName).getFile();
-            Reader reader = new FileReader(file);
-
-            CsvMapper mapper = new CsvMapper();
-            mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-//            CsvSchema schema = mapper.schemaFor(ParseToObject.class);
-//            String csv = mapper.writer(schema).writeValueAsString(value);
-
-            MappingIterator<ParseToObject> it = mapper.readerFor(ParseToObject.class).with(bootstrapSchema)
-                .readValues(reader);
-
-            while (it.hasNextValue()) {
-                System.out.println(it.nextValue());
-                // ... do something with the value
-            }
-        } catch (Exception e) {
-            logger.error("Error occurred while loading object list from file " + fileName, e);
+    public static List<Pool> read(InputStream file, List<String> poolIdInDatabase) {
+        List<ParsingContainerDTO> objects = loadObjectList(file);
+        if (verify(objects, poolIdInDatabase)) {
+            return objects.map(ParsingContainerDTO::toEmptyPool);
         }
+        return List.empty();
     }
 
-    public static void main(String[] args) {
+    private static List<ParsingContainerDTO> loadObjectList(InputStream file) {
+        try {
+            CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
+            Reader reader = new InputStreamReader(file);
+            CsvMapper mapper = new CsvMapper();
+            MappingIterator<ParsingContainerDTO> it = mapper.readerFor(ParsingContainerDTO.class).with(bootstrapSchema)
+                .readValues(reader);
+            return List.ofAll(it.readAll());
+        } catch (Exception e) {
+            logger.error("Error occurred while loading object list from file ",e);
+        }
+        return List.empty();
+    }
 
-        loadObjectList("pcoip_pools.csv");
+    private static Boolean verify(List<ParsingContainerDTO> parsedObjs, List<String> poolIdInDatabase) {
+        Map<String, ParsingContainerDTO> objects =
+            parsedObjs.toLinkedMap(parsingContainerDTO -> Tuple.of(parsingContainerDTO.getPoolId(), parsingContainerDTO));
+        return poolIdInDatabase
+            .forAll(poolId -> !objects.containsKey(poolId));
     }
 }
